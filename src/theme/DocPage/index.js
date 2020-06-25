a3dffc8bc2ad8231d5f6e457a791d8e1c58dad7f
csestito <csestito@paloaltonetworks.com>
2020-06-25 15:48:33 -0700

    fixed build issues

--- src/theme/DocPage/index.js
+++ src/theme/DocPage/index.js
@@ -5,34 +5,36 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import {MDXProvider} from '@mdx-js/react';
-
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import renderRoutes from '@docusaurus/renderRoutes';
-import Layout from '@theme/Layout';
-import DocSidebar from '@theme/DocSidebar';
-import MDXComponents from '@theme/MDXComponents';
-import NotFound from '@theme/NotFound';
-import {matchPath} from '@docusaurus/router';
-
-import styles from './styles.module.css';
+import Link from "@docusaurus/Link";
+import renderRoutes from "@docusaurus/renderRoutes";
+import { matchPath } from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { MDXProvider } from "@mdx-js/react";
+import DocSidebar from "@theme/DocSidebar";
+import Layout from "@theme/Layout";
+import MDXComponents from "@theme/MDXComponents";
+import NotFound from "@theme/NotFound";
+import React from "react";
+import styles from "./styles.module.css";
 
 function DocPage(props) {
-  const {route: baseRoute, docsMetadata, location} = props;
+  const { route: baseRoute, docsMetadata, location } = props;
   // case-sensitive route such as it is defined in the sidebar
   const currentRoute =
-    baseRoute.routes.find((route) => {
+    baseRoute.routes.find(route => {
       return matchPath(location.pathname, route);
     }) || {};
-  const {permalinkToSidebar, docsSidebars, version} = docsMetadata;
+  const { permalinkToSidebar, docsSidebars, version } = docsMetadata;
   const sidebar = permalinkToSidebar[currentRoute.path];
   const {
-    siteConfig: {themeConfig = {}} = {},
-    isClient,
+    siteConfig: { themeConfig = {} } = {},
+    siteConfig: { customFields = {} } = {},
+    isClient
   } = useDocusaurusContext();
-
-  const {sidebarCollapsible = true} = themeConfig;
+  const { sidebarCollapsible = true } = themeConfig;
+  const { docbar = {} } = customFields;
+  const { options = [] } = docbar;
 
   if (Object.keys(currentRoute).length === 0) {
     return <NotFound {...props} />;
@@ -42,7 +44,7 @@ function DocPage(props) {
     <Layout version={version} key={isClient}>
       <div className={styles.docPage}>
         {sidebar && (
-          <div className={styles.docSidebarContainer} role="complementary">
+          <div className={styles.docSidebarContainer}>
             <DocSidebar
               docsSidebars={docsSidebars}
               path={currentRoute.path}
@@ -52,6 +54,39 @@ function DocPage(props) {
           </div>
         )}
         <main className={styles.docMainContainer}>
+          <div
+            className="row row--no-gutters"
+            style={{
+              position: "sticky",
+              top: "60px",
+              zIndex: 1,
+              backgroundColor: "var(--ifm-background-color)"
+            }}
+          >
+            {options.map((menuItem, i) => (
+              <Link
+                className={
+                  "button button--outline button--secondary button--md " +
+                  (typeof window !== "undefined" &&
+                  window.location.pathname.split("/")[2] ==
+                    useBaseUrl(menuItem.to).split("/")[2]
+                    ? "button--active--tab shadow--lw"
+                    : "")
+                }
+                style={{
+                  borderRadius: "0 0 0 0",
+                  borderColor: "var(--ifm-contents-border-color)",
+                  borderWidth: "0",
+                  padding:
+                    "calc( var(--ifm-button-padding-vertical) * 1.5  ) calc( var(--ifm-button-padding-horizontal) * .65 )"
+                }}
+                key={i}
+                to={useBaseUrl(menuItem.to)}
+              >
+                {menuItem.label}
+              </Link>
+            ))}
+          </div>
           <MDXProvider components={MDXComponents}>
             {renderRoutes(baseRoute.routes)}
           </MDXProvider>
