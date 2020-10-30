c44665c30b74748fea2257441904bfe8778a2264
Eddie Beuerlein <eddie@bluemojofarm.com>
2020-10-30 21:13:37 +0000

    Fixed missing commas - thanks Marc!

--- sidebars.js
+++ sidebars.js
@@ -26,8 +26,8 @@ module.exports = {
               label: "AWS",
               items: [
                 "cloud/rql/aws/aws_ec2"
-                "cloud/rql/aws/aws_elb"
-                "cloud/rql/aws/aws_s3"
+                "cloud/rql/aws/aws_elb",
+                "cloud/rql/aws/aws_s3",
               ],
             },
             {
